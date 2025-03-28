dd2313d48d291c7ff028abe922034f9a8da8ee97
lsh4711 <dowkwll@naver.com>
2025-03-28 11:41:58 +0900

    add lsh_kb keyboard

---
+++ keyboards/lsh_kb/matrix.c
@@ -0,0 +1,13 @@
+//
+//
+//void matrix_init_custom(void) {
+//    // TODO: initialize hardware here
+//}
+//
+//bool matrix_scan_custom(matrix_row_t current_matrix[]) {
+//    bool matrix_has_changed = false;
+//
+//    // TODO: add matrix scanning routine here
+//
+//    return matrix_has_changed;
+//}
